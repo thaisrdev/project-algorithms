e060b175f67cd0b603e4fde8d783158f260073af
Thais Rosa <tsrosa18@gmail.com>
2023-06-09 19:12:38 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # permanence_period list of tuples (entry, exit)
+    counter = 0
+    if target_time is None:
+        return None
+    for time in permanence_period:
+        if not isinstance(time[0], int) or not isinstance(time[1], int):
+            return None
+        if time[0] <= target_time <= time[1]:
+            counter += 1
+    return counter
